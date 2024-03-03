7eb4e705d2a58e255ddce762a8554744c67ff6ed
Ahdad0 <abderrahimahdadabdo@gmail.com>
2024-03-03 13:58:32 +0100

    task done

--- models/place.py
+++ models/place.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
 
 
 class Place(BaseModel, Base):
