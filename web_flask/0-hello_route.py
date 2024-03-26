c80553d865d5610565e72914f0989085a7311552
Ahdad0 <abderrahimahdadabdo@gmail.com>
2024-03-26 22:39:08 +0000

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-# starts a Flask web application
+"""
+starts a Flask web application
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/", strict_slashes=False, debug=True)
 def hello():
     return "Hello HBNB!"
 
